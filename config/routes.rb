4f2732c34f04192c8acdd93882aa5ec25b2b6070
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-02 18:23:49 -0400

    Added sessions. Doesn't add much functionality. Might play with it later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+get 'show_session' => 'sessions#index'
+get 'set_session' => 'sessions#set_session'
+get 'another' => 'sessions#another'
+
 resources :houses do
   resources :characters
 end
