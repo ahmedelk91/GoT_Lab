7653a5037c6084db77ea88160567bd95b08deebb
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-02 17:56:27 -0400

    Almost at MVP. Need to add flash and possibly error-handling

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,6 +5,13 @@ def create
   redirect_to house_path(@house)
 end
 
+def destroy
+  @house = House.find(params[:house_id])
+  @character = @house.characters.find(params[:id])
+  @character.destroy
+  redirect_to house_path(@house)
+end
+
 private
 def character_params
   params.require(:character).permit(:name, :description, :img_url)
