318a1fbdfea557f3e594e82c43a127ab6611cac5
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-02 11:10:22 -0400

    Added delete feature

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -16,6 +16,12 @@ class HousesController < ApplicationController
     redirect_to @house
   end
 
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path
+  end
+
   private
   def house_params
     params.require(:house).permit(:name, :description, :img_url)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-resources :houses, only: [:index, :show, :new, :create]
+resources :houses, only: [:index, :show, :new, :create, :destroy]
 end
